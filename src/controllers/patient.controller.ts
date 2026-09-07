ebd04f54c8413442cfaf3fc5a6f4569c2fb6e543
augustinegrepo <augustinegrepo@gmail.com>
2026-09-07 14:45:37 +0800

    feat: implement patient CRUD operations

--- src/controllers/patient.controller.ts
+++ src/controllers/patient.controller.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "../../generated/prisma/client.js";
 import { prisma } from "../lib/prisma.js";
 import type { Request, Response } from "express";
 
@@ -38,6 +39,79 @@ export const getPatientProfile = async (req: Request, res: Response) => {
 
     res.json(patient);
   } catch (error) {
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const createPatient = async (req: Request, res: Response) => {
+  try {
+    const patient = await prisma.patients.create({
+      data: req.body,
+    });
+
+    res.status(201).json(patient);
+  } catch (error) {
+    console.error(error);
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const updatePatient = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+
+    const patient = await prisma.patients.update({
+      where: {
+        id: id,
+      },
+      data: req.body,
+    });
+
+    res.status(200).json(patient);
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return res.status(404).json({
+          message: "Patient not found",
+        });
+      }
+    }
+
+    res.status(500).json({
+      message: "Internal Server Error",
+    });
+  }
+};
+
+export const deletePatient = async (req: Request, res: Response) => {
+  try {
+    const id = Number(req.params.id);
+
+    const patient = await prisma.patients.delete({
+      where: {
+        id: id,
+      },
+    });
+
+    res.status(200).json(patient);
+  } catch (error) {
+    console.error(error);
+
+    if (error instanceof Prisma.PrismaClientKnownRequestError) {
+      if (error.code === "P2025") {
+        return res.status(404).json({
+          message: "Patient not found",
+        });
+      }
+    }
+
     res.status(500).json({
       message: "Internal Server Error",
     });
--- src/routes/patient.routes.ts
+++ src/routes/patient.routes.ts
@@ -1,9 +1,19 @@
-import { getPatients } from "../controllers/patient.controller.js";
+import {
+  getPatients,
+  getPatientProfile,
+  createPatient,
+  updatePatient,
+  deletePatient,
+} from "../controllers/patient.controller.js";
+
 import { Router } from "express";
 
 const router = Router();
 
 router.get("/", getPatients);
 router.get("/:id", getPatientProfile);
+router.post("/", createPatient);
+router.patch("/:id", updatePatient);
+router.delete("/:id", deletePatient);
 
 export default router;
